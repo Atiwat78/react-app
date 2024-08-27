9ea08298d7c24aaea7b580a6d83e9f01179dc4cf
twk56 <thanawatklomkliew@gmail.com>
2024-08-27 16:30:44 +0700

    new file:   .vscode/settings.json 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/Button.js 	new file:   src/Mycss.css 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
+import React, { useState } from 'react';
 
 function App() {
+  const name = "Thanawat Klomkliew";
+  const [count, setCount] = useState(0);
+
+  const employee = [
+    { name: "AAA", email: "aaa@gmail.com", age: 19 },
+    { name: "BBB", email: "bbb@gmail.com", age: 20 },
+    { name: "CCC", email: "ccc@gmail.com", age: 21 }
+  ];
+  console.log(employee);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}> + </button>
+      <button onClick={() => setCount(count - 1)}> - </button>
+
+
+      <ul>
+        {employee.map((em, index) => (
+          <li key={index}>
+            ชื่อพนักงาน : {em.name} | อีเมล์ : {em.email} | อายุ : {em.age}
+          </li>
+        ))}
+      </ul>
+      <h1>
+        Hello {name}
+      </h1>
+      <h5>
+        <Button text="OK" />
+        <Button text="Remove" />
+      </h5>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,12 @@
+import './Mycss.css';
+function Button({text}) {
+    return ( 
+        <>
+        <button className='btn01'>
+            {text}
+        </button>
+        </>
+     );
+}
+
+export default Button;
